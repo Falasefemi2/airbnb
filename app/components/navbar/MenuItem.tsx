d19c9b1afa4a33dc30835df1b9e62255e8eb2dd9
Falase Femi <femifalase228@gmail.com>
2024-01-23 15:04:58 +0100

    feat((navbar)): implement responsive design for site navigation

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { MenuItemProps } from "@/app/types"
+
+const MenuItem:React.FC<MenuItemProps> = ({onClick, label}) => {
+  return (
+    <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+        {label}
+    </div>
+  )
+}
+
+export default MenuItem
