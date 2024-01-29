1f169b4215b922abad391e4f68e795e585192b51
Falase Femi <femifalase228@gmail.com>
2024-01-29 11:12:58 +0100

    feat(added styling for the input component):

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -1,10 +1,33 @@
 "use client"
 
+import { BiDollar } from "react-icons/bi";
+import { InputProps } from "../../types";
 
 
-const Input = () => {
+
+const Input:React.FC<InputProps> = ({id,label,type="text", disabled,formatPrice,register,required,errors}) => {
     return (
-        <div></div>
+        <div className="w-full relative">
+            {formatPrice && (
+                <BiDollar size={24} className="text-neutral-700 absolute top-5 left-2" />
+            )}
+            <input 
+            id={id}
+            disabled={disabled}
+            {...register(id, {required})}
+            placeholder=" "
+            type={type}
+            className={`
+            peer
+            w-full
+            p-4
+            pt-6
+            font-light
+            bg-white
+            border-2
+            `}
+            />
+        </div>
     )
 }
 
