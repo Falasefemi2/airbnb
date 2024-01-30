b7422a00848104cf9d73edf7d649aaf8f6eab6b8
Falase Femi <femifalase228@gmail.com>
2024-01-30 13:08:06 +0100

    fix(types):modifiy the types for user

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,6 @@
 import { IconType } from "react-icons";
 import {FieldValues, UseFormRegister, FieldErrors} from 'react-hook-form'
+import { User } from "@prisma/client";
 
 export interface ContainerProps {
     children: React.ReactNode
@@ -60,4 +61,13 @@ export interface InputProps {
     required?: boolean;
     register: UseFormRegister<FieldValues>,
     errors: FieldErrors
+}
+
+export type SafeUser = Omit<
+User,
+"createdAt" | "updatedAt" | "emailVerified"
+> & {
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null;    
 }
