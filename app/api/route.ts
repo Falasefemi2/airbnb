e1efd9633063989ea6b43d477a40f7400bbf95d7
Falase Femi <femifalase228@gmail.com>
2024-02-04 16:25:57 +0100

    deleted the route.ts

--- app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./auth/[...nextauth]/route";
-
-
-
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST}
