9335b52e4fcf43a619042c7d9091ca6afeb0cf2a
Falase Femi <femifalase228@gmail.com>
2024-02-16 11:58:33 +0100

    feat(leaflet map)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../input/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../input/CountrySelect";
+import Map from "../Map";
 
 enum STEPS {
     CATEGORY = 0,
@@ -103,6 +104,7 @@ const RentModal = () => {
                 value={location}
                 onChange={(value) => setCustomValue('location', value)}
                 />
+                <Map />
             </div>
         )
     }
