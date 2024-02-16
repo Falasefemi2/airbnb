282a3ede52726da0922840cf3da89bddd7024700
Falase Femi <femifalase228@gmail.com>
2024-02-16 10:49:37 +0100

    feat(option to select country)

--- app/components/input/CountrySelect.tsx
+++ app/components/input/CountrySelect.tsx
@@ -1,11 +1,48 @@
 "use client";
 
-const CountrySelect = () => {
-    return (  
+import useCountries from '@/app/hooks/useCountries';
+import Select from 'react-select';
+
+export type CountySelectValue = {
+    flag: string;
+    label: string;
+    latlng: number[];
+    region: string;
+    value: string;
+}
+
+interface CountrySelectProps {
+    value?: CountySelectValue;
+    onChange: (value: CountySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
+
+    const { getAll } = useCountries();
+    return (
         <div>
-            
+            <Select
+                placeholder="Anywhere"
+                isClearable
+                options={getAll()}
+                value={value}
+                onChange={(value) => onChange(value as CountySelectValue)}
+                formatOptionLabel={(option: any) => (
+                    <div className='flex flex-row items-center gap-3'>
+                        <div>
+                            {/* {option.flag} */}
+                            <img src={option.flag} alt={option.label} width="20" height="20" /> {/* Displaying flag as an image */}
+
+                        </div>
+                        <div>
+                            {option.label},
+                            <span className='text-neutral-500 ml-1'>{option.region}</span>
+                        </div>
+                    </div>
+                )}
+            />
         </div>
     );
 }
- 
+
 export default CountrySelect;
