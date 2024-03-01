cc89233c4cea44666649e8b66298fcb0e2b3f4b5
Falase Femi <femifalase228@gmail.com>
2024-03-01 11:48:29 +0100

    fix(css grid)

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,6 @@ export default async function Home() {
     <ClientOnly>
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        <div>
           {listings.map((listing: any) => {
             return (
               <ListingCard 
@@ -30,7 +29,6 @@ export default async function Home() {
               />
             )
           })}
-        </div>
       </div>
     </Container>
     </ClientOnly>
