ed1102b4e9d0e8c80227a27ca0d2bb5f502ee802
Falasefemi2 <femifalase228@gmail.com>
2024-03-04 22:50:18 +0100

    feat(favorite housing)

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
     listingId: string;
@@ -9,11 +10,13 @@ interface HeartButtonProps {
 }
 
 const HeartButton:React.FC<HeartButtonProps> = ({listingId, currentUser}) => {
-    const hasFavorited = false;
-    const toggleFavourite = () => {};
+    const {hasFavorited, toggleFavorite} = useFavorite({
+        listingId, 
+        currentUser
+    })
 
     return (
-        <div onClick={toggleFavourite} className="relative hover:opacity-80 transition cursor-pointer">
+        <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
             <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]" />
             <AiFillHeart size={24} className={hasFavorited ? 'text-rose-500'  : 'fill-neutral-500/70'} />
         </div>
