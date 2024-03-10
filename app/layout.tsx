093e24946549d3104c754dc5fc02eafa070a0f35
Falasefemi2 <femifalase228@gmail.com>
2024-03-10 22:36:34 +0100

    feat(search function)

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import RentModal from "./components/modals/RentModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
+import SearchModal from "./components/modals/SearchModal";
 
 
 
@@ -32,16 +33,17 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-        <ToasterProvider />
-        <RentModal />
-        <LoginModal />
-        <RegisterModal />
-        <Navbar currentUser={currentUser} />
+          <ToasterProvider />
+          <SearchModal />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">
-        {children}
+          {children}
         </div>
-        </body>
+      </body>
     </html>
   );
 }
